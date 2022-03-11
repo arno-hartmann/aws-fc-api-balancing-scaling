2cc74b1bcbfdb712fa9f29394709658c96667a66
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-11 14:52:22 +0100

    Init funktionen und write answer_array

--- getFilesFromS3Util.py
+++ getFilesFromS3Util.py
@@ -1,13 +1,35 @@
 import boto3
+import json
+import os
+
 
 def getFilesFromS3 (bucketname):
    s3client = boto3.client('s3')
 
-   response = s3client.list_objects(
+   response_list = s3client.list_objects(
       Bucket = bucketname
       )
-   counter = 0
-   for item in response["Contents"]:
-      print(response["Contents"][counter]['Key'])
-      counter +=1
 
+
+   counter = 0
+   for item in response_list["Contents"]:
+
+      key = response_list["Contents"][counter]['Key']
+      
+      response_content = s3client.get_object(
+         Bucket = bucketname,
+         Key = key
+         )
+
+      text = response_content['Body'].read()
+      
+      words = text.split()
+      numberOfWords = len(words)
+   
+      #message = 'Number of words in text file {} is {}'.format(key, numberOfWords)
+      #print(message)
+
+      answer_array = [counter, "\"key\" :", key, "\"words\":", numberOfWords ]
+      print(answer_array)
+      counter +=1
+      return answer_array
