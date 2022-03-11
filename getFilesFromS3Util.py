8aa28db4c21c7f085fe609217d2dbcbc9b9f7732
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-11 12:46:32 +0100

    Init function get Filename

--- getFilesFromS3Util.py
+++ getFilesFromS3Util.py
@@ -2,11 +2,12 @@ import boto3
 
 def getFilesFromS3 (bucketname):
    s3client = boto3.client('s3')
-#   print(s3client)
-#   print(type(s3client))
 
    response = s3client.list_objects(
       Bucket = bucketname
       )
-   print(response["Contents"][0]['Key'])
+   counter = 0
+   for item in response["Contents"]:
+      print(response["Contents"][counter]['Key'])
+      counter +=1
 
