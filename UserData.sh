f14c2a9657fb6a1ffc4920f9519aae35800a1f6b
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-11 17:44:15 +0100

    End the day, User Data not working yet

---
+++ UserData.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+yum update -y
+
+yum install -y python
+yum install -y pip
+pip3 install flask
+
+mkdir fridaychallenge
+
+cd fridaychallenge
+python3 -m venv venv
+
+. venv/bin/activate
+
+
+
+mkdir flaskr
+
+#cat flaskr/__init__.py <<EOF
+## wget -> github.com
+
+#cat getFilesFromS3Util.py <<EOF
+
+
+
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run --host=0.0.0.0 --port=80
