172b36c02b64a4aed93a7d68ce048634a3d244f3
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-13 17:19:50 +0100

    Update UserData.sh wget, unzip, URL to github, output

--- UserData.sh
+++ UserData.sh
@@ -2,28 +2,24 @@
 
 yum update -y
 
+yum install -y wget
+yum install -y unzip
 yum install -y python
 yum install -y pip
 pip3 install flask
 
 
-mkdir fridaychallenge
+output : { all : '| tee -a /var/log/cloud-init-output.log' }
 
-cd fridaychallenge
+wget https://github.com/arno-hartmann/aws-fc-api-balancing-scaling/archive/refs/heads/main.zip
+
+unzip main 
+
+cd aws-fc-api-balancing-scaling-main
+
 python3 -m venv venv
 
 . venv/bin/activate
-
-
-
-mkdir flaskr
-
-#cat flaskr/__init__.py <<EOF
-## wget -> github.com
-
-#cat getFilesFromS3Util.py <<EOF
-
-
 
 export FLASK_APP=flaskr
 export FLASK_ENV=development
