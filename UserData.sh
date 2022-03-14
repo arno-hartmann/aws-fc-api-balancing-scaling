1da7b5c0df4f44b04f7e7f1b10947354663a99f5
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-14 12:44:00 +0100

    Update UserData

--- UserData.sh
+++ UserData.sh
@@ -6,7 +6,7 @@ yum install -y wget
 yum install -y unzip
 yum install -y python
 yum install -y pip
-pip3 install flask
+pip3 install flask boto3
 
 
 output : { all : '| tee -a /var/log/cloud-init-output.log' }
